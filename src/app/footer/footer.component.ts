08be06918828996165a63a4e6dd615ed84db66c4
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-15 21:16:42 +0700

    count dow time, hackernew, like , text input done

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+  name = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  // tslint:disable-next-line:typedef
+  showEvent(envent) {
+    this.name = envent.target.value;
+  }
 }
